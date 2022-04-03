90e8477b90ff62edc6c96b3347af0d866a18bf6b
danton03 <37675973+danton03@users.noreply.github.com>
2022-04-02 22:54:07 -0300

    Finalização do projeto sem o bônus

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 let valorPrato = 0;
 let valorBebida = 0;
 let valorSobremesa = 0;
-
+let total = 0;
 
 function selecionarPrato(prato) {
   const selecionado = document.querySelector(".prato .selecionado");
@@ -21,9 +21,10 @@ function selecionarPrato(prato) {
   //Pegando o valor do prato e formatando para fazer os cálculos
   valorPrato = prato.children.item(3).innerText;
   valorPrato = Number(valorPrato.replace("R$","").replace(",","."));
-  
-  //chama a função para verificar se pode ativar o botão de fechar o pedido
-  finalizarPedido();
+  valorPrato = valorPrato.toFixed(2);
+ 
+  //chama a função que checa se pode habilitar o botão
+  habilitaBotao();
 }
 
 
@@ -45,10 +46,12 @@ function selecionarBebida(bebida) {
 
   valorBebida = bebida.children.item(3).innerText;
   valorBebida = Number(valorBebida.replace("R$","").replace(",","."));
+  valorBebida = valorBebida.toFixed(2);
 
-  //chama a função para verificar se pode ativar o botão de fechar o pedido
-  finalizarPedido();
+  //chama a função que checa se pode habilitar o botão
+  habilitaBotao();
 }
+
 
 function selecionarSobremesa(sobremesa) {
   const selecionado = document.querySelector(".sobremesa .selecionado");
@@ -67,14 +70,13 @@ function selecionarSobremesa(sobremesa) {
 
   valorSobremesa = sobremesa.children.item(3).innerText;
   valorSobremesa = Number(valorSobremesa.replace("R$","").replace(",","."));
+  valorSobremesa = valorSobremesa.toFixed(2);
 
-  //chama a função para verificar se pode ativar o botão de fechar o pedido
-  finalizarPedido();
+  //chama a função que checa se pode habilitar o botão
+  habilitaBotao();
 }
 
-
-
-function finalizarPedido() {
+function habilitaBotao() {
   const pratoSelecionado = document.querySelector(".prato .selecionado");
   const bebidaSelecionada = document.querySelector(".bebida .selecionado");
   const sobremesaSelecionada = document.querySelector(".sobremesa .selecionado");
@@ -82,6 +84,33 @@ function finalizarPedido() {
   if (pratoSelecionado !== null && bebidaSelecionada !== null && sobremesaSelecionada !== null) {
     const botao = document.querySelector(".pedir").children.item(0);
     botao.disabled = false;
-    botao.textContent = "Finalizar Pedido"
+    botao.textContent = "Finalizar Pedido";
   }
+}
+
+function finalizarPedido() {
+  const pratoSelecionado = document.querySelector(".prato .selecionado");
+  const bebidaSelecionada = document.querySelector(".bebida .selecionado");
+  const sobremesaSelecionada = document.querySelector(".sobremesa .selecionado");
+
+  //Calcula o valor final do pedido
+  //com apenas 2 dígitos decimais
+  total = (Number(valorPrato) + Number(valorBebida) + Number(valorSobremesa)).toFixed(2);
+  let totalTxt = total.toString().replace('.',","); //troca novamente o "ponto" por "vírgula"
+
+  //Pegando o nome dos itens pedidos
+  const pratoPedido = pratoSelecionado.children.item(1).textContent;
+  const bebidaPedida = bebidaSelecionada.children.item(1).textContent;
+  const sobremesaPedida = sobremesaSelecionada.children.item(1).textContent;
+
+  //Texto do pedido que será enviado via whatsapp
+  const uri = `Olá, gostaria de fazer o pedido:\n
+  - Prato: ${pratoPedido}\n
+  - Bebida: ${bebidaPedida}\n
+  - Sobremesa: ${sobremesaPedida}\n\n
+  Total: R$ ${totalTxt} `
+
+  //URI encodada para funcionar no navegador
+  const uriEncoded = encodeURIComponent(uri);
+  window.open(`https://wa.me/5586988660071?text=${uriEncoded}`,'_blank');
 }
